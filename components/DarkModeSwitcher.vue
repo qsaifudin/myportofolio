106c11d96e501af104d575387d5b8f5104e15914
qsaifudin <duafinis@gmail.com>
2023-08-17 14:01:16 +0700

    .

--- components/DarkModeSwitcher.vue
+++ components/DarkModeSwitcher.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-btn icon @click="toggleDarkMode">
-      <div v-if="nightMode">
+      <div v-if="!nightMode">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
