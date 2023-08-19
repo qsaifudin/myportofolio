5e58f2f73ec97c33ecf480fbdd9484a890ede357
qsaifudin <duafinis@gmail.com>
2023-08-19 11:53:26 +0700

    .

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,41 +1,42 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
+  <v-container grid-list-xs class="pa-5 pt-4 myPrimary--text text-center">
+    <h1
+      v-if="error.statusCode === 404"
+      class="text-center helvetica-bold font-extrabold mb-4"
+    >
+      {{ pageNotFound }} mau kemana bray
     </h1>
     <h1 v-else>
       {{ otherError }}
     </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
-  </v-app>
+    <NuxtLink to="/"> Home page </NuxtLink>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'EmptyLayout',
-  layout: 'empty',
+  name: "EmptyLayout",
+  layout: "empty",
   props: {
     error: {
       type: Object,
-      default: null
-    }
+      default: null,
+    },
   },
-  data () {
+  data() {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
-    }
+      pageNotFound: "404 Not Found",
+      otherError: "An error occurred",
+    };
   },
-  head () {
+  head() {
     const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
     return {
-      title
-    }
-  }
-}
+      title,
+    };
+  },
+};
 </script>
 
 <style scoped>
