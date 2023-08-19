270c973e0fce0d457771a405e10dbf76139c275a
qsaifudin <duafinis@gmail.com>
2023-08-19 19:43:51 +0700

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico?v=2" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
